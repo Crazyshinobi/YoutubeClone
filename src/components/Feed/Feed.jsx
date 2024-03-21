9e1f36a22d87f7dd2ff967c6a1bbafecd2993501
crazyshinobi <chetansingh410@gmail.com>
2024-03-21 12:23:59 +0530

    Initial commit

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+import { Link } from "react-router-dom";
+import { API_KEY } from "../../data";
+import { valueConverter } from "../../data";
+
+import "./Feed.css";
+
+const Feed = ({ category }) => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const videolist_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+
+    await fetch(videolist_url)
+      .then((response) => response.json())
+      .then((data) => setData(data.items));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [category]);
+
+  return (
+    <div className="feed">
+      {data.map((item, index) => {
+        return (
+          <Link
+            key={index}
+            to={`video/${item.snippet.categoryId}/${item.id}`}
+            className="card"
+          >
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <h2>{item.snippet.title}</h2>
+            <h3>{item.snippet.channelTitle}</h3>
+            <p>
+              {valueConverter(item.statistics.viewCount)} views &bull;{" "}
+              {moment(item.snippet.publishedAt).fromNow()}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Feed;
