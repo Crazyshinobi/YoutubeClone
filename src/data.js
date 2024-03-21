471f8e08c5cfac8d8cb8aaa69aafd784e73138b7
crazyshinobi <chetansingh410@gmail.com>
2024-03-21 12:24:40 +0530

    Initial commit

--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-export const API_KEY = "AIzaSyACVtVd0bNtXoyyVUpUffrGeG6OU2PAPo4";
-
 export const valueConverter = (value) => {
   if (value >= 1000000) {
     return Math.floor(value / 1000000) + "M";
